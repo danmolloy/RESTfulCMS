093e3012d3d259dae03386756c9541d11b8daa73
Daniel Molloy <danielmolloy_6@icloud.com>
2024-04-27 22:07:47 +0100

    added to allowed hosts

--- mycms/settings.py
+++ mycms/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://restfulcms-production.up.railway.app"]
+ALLOWED_HOSTS = ["127.0.0.1", "restfulcms-production.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ["https://restfulcms-production.up.railway.app"]
 
 CORS_ALLOWED_ORIGINS = [
